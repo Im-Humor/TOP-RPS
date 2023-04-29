399b44d048dae29d14598d4c0ee597ecfeb960bf
Im-Humor <orangite@gmail.com>
2023-04-29 11:55:39 -0700

    add DOM elements containing win data and add game logic to update win counts and the eventual win conditions

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ function getComputerChoice() {
     return choiceArray[choiceNum]
 }
 
+let playerWins = 0;
+let computerWins = 0;
+
 function playRound(playerSelection, computerSelection) {
     const winString = "Congratulations, you won!";
-    const tieString = "You have tied your opponent! Try again!"
-    const loseString = "You lost! Try again!"
+    const tieString = "You have tied your opponent! Try again!";
+    const loseString = "You lost! Try again!";
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        playerWins += 1;
         return(winString);
     }
     else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        playerWins += 1;
         return(winString);
     }
     else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        playerWins += 1;
         return(winString);
     }
     else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
@@ -27,10 +33,28 @@ function playRound(playerSelection, computerSelection) {
         return(tieString);
     }
     else {
+        computerWins += 1;
         return(loseString);
     }
 }
 
-function game() {
-    console.log(playRound(prompt("Do you select rock, paper, or scissors?"), getComputerChoice()));
+const buttons = document.querySelectorAll('button');
+
+const game = (event) => {
+    console.log(playRound(event.target.textContent, getComputerChoice()))
+    const playerWinCount = document.querySelector('.player-win-count');
+    const cpuWinCount = document.querySelector('.cpu-win-count');
+    const winner = document.querySelector('.winner');
+    playerWinCount.textContent = playerWins;
+    cpuWinCount.textContent = computerWins;
+    if (playerWins >= 5) {
+        winner.textContent = 'Player wins!';
+        buttons.forEach(button => button.removeEventListener('click', game));
+    }
+    else if (computerWins >= 5) {
+        winner.textContent = "CPU wins!";
+        buttons.forEach(button => button.removeEventListener('click', game));
+    }
 }
+
+buttons.forEach(button => button.addEventListener('click', game));
